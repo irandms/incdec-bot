ae393a6b08015baff76ca6e247af0e71ac2901b5
Spencer Moss <radio@another.computer>
2017-11-29 06:09:50 -0800

    now handling multiuser mentions

--- adny-bot.py
+++ adny-bot.py
@@ -21,26 +21,49 @@ def myscore(bot, update):
             'Hello {}, your score is {}.'.format(username, score))
 
 def score(bot, update):
-    update.message.reply_text('to be written')
+    mentioned_users = set()
+    for mention in update.message.entities:
+        if mention.type != 'mention':
+            continue
+        mention_begin = mention.offset
+        mention_end = mention_begin + mention.length
+
+        username = update.message.text[mention_begin+1:mention_end]
+        mentioned_users.add(username)
+
+    reply_str = ''
+    for user in mentioned_users:
+        reply_str += '{} has a score of {}.\n'.format(user, db[user])
+    update.message.reply_text(reply_str)
 
 def update_score(bot, update):
+    mentioned_users = set()
     for mention in update.message.entities:
-        if mention.type == 'mention':
-            mention_begin = mention.offset
-            mention_end = mention_begin + mention.length
+        if mention.type != 'mention':
+            continue
+        mention_begin = mention.offset
+        mention_end = mention_begin + mention.length
 
         username = update.message.text[mention_begin+1:mention_end]
 
-        if '--' in update.message.text:
+        action = update.message.text[mention_end:]
+        action = action.strip()
+        action = action[:2]
+
+        if '--' in action:
             db[username] -= 1
-        else:
+            mentioned_users.add(username)
+        elif '++' in action:
             db[username] += 1
+            mentioned_users.add(username)
 
-        with open('adny-bot-db.json', 'w') as dbfile:
-            json.dump(db, dbfile)
+    with open('adny-bot-db.json', 'w') as dbfile:
+        json.dump(db, dbfile)
 
-        update.message.reply_text(
-                '{} is now at a score of {}.'.format(username, db[username]))
+    reply_str = ''
+    for user in mentioned_users:
+        reply_str += '{} now has a score of {}.\n'.format(user, db[user])
+    update.message.reply_text(reply_str)
 
 if "TELEGRAM_BOT_API_KEY" in os.environ:
     updater = Updater(os.environ.get("TELEGRAM_BOT_API_KEY"))
