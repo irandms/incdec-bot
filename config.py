857044c52da5dc6f6bbb6cb8e121ab73fd249e72
Andy Williams <andy@another.computer>
2018-05-17 16:34:56 -0700

    Keeps track of who is sorted into what subgroup
    Uses the first two letters of each subgroup. ChE of ChemE will always go to CH. All or Everyone will return the same things as well.

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
 ROTATE_MAX_CHARS = 15
+
+SUBGROUPS = {'EE': ['DiddleDad', 'fgsrfug', 'Selfishshellfish', 'aaronschraner'],
+             'CS': ['tolvstaa', 'Codification', 'codysseus'],
+             'CH': ['TheChemE'],
+             'BO': ['adny_bot'],
+             'AN': ['adny_bot', 'TheChemE', 'tolvstaa'],
+             'CO': ['Codification', 'codysseys'],
+             'GR': ['tolvstaa', 'Codification'],
+             'NE': ['irandms'],
+             'AL': []}
+
+for users in SUBGROUPS.values():
+    SUBGROUPS['AL'] += users
+
+SUBGROUPS['AL'] = list(set(SUBGROUPS['AL']))
+SUBGROUPS['EV'] = SUBGROUPS['AL']
